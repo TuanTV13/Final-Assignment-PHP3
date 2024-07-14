0f156b7762a6781c0c10d2e0b40d0b3cee1f7373
TuanTV13 <tvtuan01032004@gmail.com>
2024-07-14 21:04:41 +0700

    js

--- resources/views/admins/layouts/partials/js.blade.php
+++ resources/views/admins/layouts/partials/js.blade.php
@@ -1,22 +1,22 @@
-<script src="/admins/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/admins/assets/libs/simplebar/simplebar.min.js"></script>
-<script src="/admins/assets/libs/node-waves/waves.min.js"></script>
-<script src="/admins/assets/libs/feather-icons/feather.min.js"></script>
-<script src="/admins/assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
-<script src="/admins/assets/js/plugins.js"></script>
+<script src="{{ asset('admins/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('admins/assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('admins/assets/libs/node-waves/waves.min.js') }}"></script>
+<script src="{{ asset('admins/assets/libs/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('admins/assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+<script src="{{ asset('admins/assets/js/plugins.js') }}"></script>
 
 <!-- apexcharts -->
-<script src="/admins/assets/libs/apexcharts/apexcharts.min.js"></script>
+<script src="{{ asset('admins/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
 <!-- Vector map-->
-<script src="/admins/assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
-<script src="/admins/assets/libs/jsvectormap/maps/world-merc.js"></script>
+<script src="{{ asset('admins/assets/libs/jsvectormap/js/jsvectormap.min.js') }}"></script>
+<script src="{{ asset('admins/assets/libs/jsvectormap/maps/world-merc.js') }}"></script>
 
 <!-- Dashboard init -->
-<script src="/admins/assets/js/pages/dashboard-analytics.init.js"></script>
+<script src="{{ asset('admins/assets/js/pages/dashboard-analytics.init.js') }}"></script>
 
 <!-- App js -->
-<script src="/admins/assets/js/app.js"></script>
+<script src="{{ asset('admins/assets/js/app.js') }}"></script>
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
